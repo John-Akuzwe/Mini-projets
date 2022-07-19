af838f6d6b37129e04b0bba8a996ddf307043c88
John-Akuzwe <akuzwe2000@gmail.com>
2022-07-19 14:51:17 +0200

    encore plus simplifier

--- Guessing Game/Test.js
+++ Guessing Game/Test.js
@@ -2,16 +2,14 @@ function guess () {
     let chifre = document.getElementById("chiffre").value;
     console.log("Your number was " + chifre);
 
-    let playerGuess = chifre
-
     const numberToGuess = Math.ceil(Math.random () * 5);
     console.log("The right answer was " + numberToGuess);
 
-    if (playerGuess == numberToGuess) {
+    if (chifre == numberToGuess) {
         alert("Congratulations! It was " + numberToGuess + ". Must be some luck. Do it again and I'll give you your five bucks." );
 
-    }else if (playerGuess > 5 ) {
-        alert("Do you know how to read ? I told you it was beetwen 0 and 5!") 
+    }else if (chifre > 5 ) {
+        alert("Do you know how to read ? I told you it was beetwen 1 and 5!") 
 
     }else {
         alert("Useless, it was " + numberToGuess + ". Just quit you'll never get it");
