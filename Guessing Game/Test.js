b8fce31c7de24bb3c6fea00d8e98d2d2707e56c5
John-Akuzwe <akuzwe2000@gmail.com>
2022-07-19 15:01:42 +0200

    modif condition else if

--- Guessing Game/Test.js
+++ Guessing Game/Test.js
@@ -1,17 +1,21 @@
-function guess () {
-    let chifre = document.getElementById("chiffre").value;
-    console.log("Your number was " + chifre);
+function guess() {
+  let chifre = document.getElementById("chiffre").value;
+  console.log("Your number was " + chifre);
 
-    const numberToGuess = Math.ceil(Math.random () * 5);
-    console.log("The right answer was " + numberToGuess);
+  const numberToGuess = Math.ceil(Math.random() * 5);
+  console.log("The right answer was " + numberToGuess);
 
-    if (chifre == numberToGuess) {
-        alert("Congratulations! It was " + numberToGuess + ". Must be some luck. Do it again and I'll give you your five bucks." );
-
-    }else if (chifre > 5 ) {
-        alert("Do you know how to read ? I told you it was beetwen 1 and 5!") 
-
-    }else {
-        alert("Useless, it was " + numberToGuess + ". Just quit you'll never get it");
-    }   
+  if (chifre == numberToGuess) {
+    alert(
+      "Congratulations! It was " +
+        numberToGuess +
+        ". Must be some luck. Do it again and I'll give you your five bucks."
+    );
+  } else if (chifre > 5 || chifre == 0) {
+    alert("Do you know how to read ? I told you it was beetwen 1 and 5!");
+  } else {
+    alert(
+      "Useless, it was " + numberToGuess + ". Just quit you'll never get it"
+    );
+  }
 }
